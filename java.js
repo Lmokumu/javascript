e9548c8f328d2ec66c553b4211adbdccb41cc30c
marylyne <leahmarylyne@gmail.com>
2022-06-23 10:00:02 +0300

    first javascript

---
+++ java.js
@@ -0,0 +1,11 @@
+let person = 'Name: Leah Okumu';
+let height = 'Height: 5.4ft';
+let country = 'Country: Kenya';
+//console.log(person, firstName, lastName);
+//console.log(height, hgt);
+//console.log(country, name)
+
+document.getElementById("person").innerHTML = person;
+document.getElementById("height").innerHTML = height;
+document.getElementById("country").innerHTML = country;
+//document.getElementById("demo").innerHTML = height;
